1951804fb3d441c0f4e83fea83382d452b7858ba
SurbhiBhutani <surbhi.bhutani@dish.com>
2023-02-13 15:24:36 +0000

    added more tests for validation funcs

--- tests/test_datatype_validation.py
+++ tests/test_datatype_validation.py
@@ -24,4 +24,31 @@ def test_integer_check():
     actual = rulebook.integer_check(integer_data_df,integer_column)
     expected = 5, 'integers',[2,3,4,5]
     assert actual == expected
+
+def test_short_check():
+    '''Tests the short datatype, testdata is created within this method'''
+    short_data_df = spark.createDataFrame(pd.DataFrame(
+        [[0,'32768'],
+         [1,'-32769'],
+         [2,'1'],
+         [3,'1.0'],
+         [4,'2e8']], columns= ['ROW_ID','shorts']))
+    short_column = 'shorts'
+    actual = rulebook.short_check(short_data_df,short_column)
+    expected = 6,'shorts',[0,1,4]
+    assert actual == expected
+
+def test_long_check():
+    '''Tests the long datatype, testdata is created within this method'''
+    long_data_df = spark.createDataFrame(pd.DataFrame(
+        [[0,'9223372036854775809'],
+         [1,'-9223372036854775809'],
+         [2,'1'],
+         [3,'1.0'],[4,'2e8']], columns = ['ROW_ID','longs']))
+    long_column = 'longs'
+    actual = rulebook.long_check(long_data_df,long_column)
+    expected = 7, 'longs', [0,1,4]
+    assert actual == expected
+    
+
     
