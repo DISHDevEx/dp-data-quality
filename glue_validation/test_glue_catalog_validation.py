26958aab452f7e997a81d70367117cc0e073aec6
John Liu <zheng.liu@dish.com>
2023-03-10 10:58:30 -0700

    Update: not able to scan Glue Database from SageMaker.

--- glue_validation/test_glue_catalog_validation.py
+++ glue_validation/test_glue_catalog_validation.py
@@ -14,25 +14,27 @@ from glue_catalog_validation import *
 #     pass
 #     # From glue job instance, not able to test in sagemaker
 
-@pytest.mark.test_glue_database_list
-@pytest.mark.parametrize(
-    "glue_database_name",
-    [
-        "lambdaglue2"
-    ]
-)
-def test_glue_database_list_correct(glue_database_name):
-    """
-    Test function bucket_validation with correct input:
-        glue_database_name
-    Pass criteria:
-        result_list equals to expected_list
-    """
-    expected_list = ['boost_xp_aggregated',
-                        'boost_xp_data',
-                        'boost_xp_voice',
-                        'd_use1_dish_5g_core_orch_b_fhaul_cust_fm_a',
-                        'd_use1_dish_5g_core_orch_b_fhaul_cust_fm_ab_b_b_b_b_b_b_b_b',
-                        'unsaved'].sort()
-    result_list = glue_database_list(glue_database_name).sort()
-    assert expected_list == result_list
+# # Because there is no correct permission from SageMaker to Glue Database,
+# # it is not possible to test it here.
+# @pytest.mark.test_glue_database_list
+# @pytest.mark.parametrize(
+#     "glue_database_name",
+#     [
+#         "lambdaglue2"
+#     ]
+# )
+# def test_glue_database_list_correct(glue_database_name):
+#     """
+#     Test function bucket_validation with correct input:
+#         glue_database_name
+#     Pass criteria:
+#         result_set equals to expected_set
+#     """
+#     expected_set = set(['boost_xp_aggregated',
+#                         'boost_xp_data',
+#                         'boost_xp_voice',
+#                         'd_use1_dish_5g_core_orch_b_fhaul_cust_fm_a',
+#                         'd_use1_dish_5g_core_orch_b_fhaul_cust_fm_ab_b_b_b_b_b_b_b_b',
+#                         'unsaved'])
+#     result_set = set(glue_database_list(glue_database_name))
+#     assert expected_set == result_set
